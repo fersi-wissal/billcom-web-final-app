38446682c7d2b4540cf749f4d6e93b5ef1462f55
Fersi Wissal <ferciwissal@gmail.com>
2021-09-27 14:32:38 +0200

    27/09/2021/14:32

--- src/main/java/com/billcom/app/config/DatabaseConfig.java
+++ src/main/java/com/billcom/app/config/DatabaseConfig.java
@@ -18,13 +18,13 @@ import javax.sql.DataSource;
 //@Configuration
 
 public class DatabaseConfig {
-/*@Value("${spring.datasource.url}")
+@Value("${spring.datasource.url}")
  private String dbUrl;
  @Bean
  public DataSource dataSource() {
  HikariConfig config = new HikariConfig();
  config.setJdbcUrl(dbUrl);
  return new HikariDataSource(config);
- }*/
+ }
 }
 
